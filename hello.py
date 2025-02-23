7346153d6929f441fac028a33faa73fc913f4618
Livia Bottoni <livia.bottoni@gmail.com>
2025-02-23 22:55:03 +0100

    fetching also product type - still working on timers and other info

--- hello.py
+++ hello.py
@@ -1,27 +1,36 @@
 # Module that makes HTTP requests to URL and returns response 
 import requests
-# Module that provides methods and phrases for guiding, searching and changin parse tree
+# Module that provides methods and phrases for guiding, searching and changing parse tree
 from bs4 import BeautifulSoup
-# Pandas for writing DataFrame
+# URL parse to fetch product type
+from urllib.parse import urlparse
+# Pandas for writing DataFrame & Excel
 import pandas as pd
-# Time to insert delays - otherwise, too many requests get blocked
-import time
+# Random & time to add delays
+from random import randint
+from time import sleep
 
 # Start from product catalogue page with pagination parameter
-url = 'https://www.eurocontrol.int/what-we-offer?page='
+baseurl = 'https://www.eurocontrol.int'
+prodsurl = '/what-we-offer?page='
 
 # Number of pages (could also be made dynamic)
 pages = 25
 
-# Arrays we will store information
+# Declaring variables for later
 plinks = []
+ptype = []
 pnames = []
 pacts = []
+col1 = "Link"
+col2 = "Type"
+col3 = "Page title"
+col4 = "Activities"
 
 # Loop through all pages to fetch URLs of each product/tool
 for page in range(0, pages):
     # GET request
-    r = requests.get(url + str(page))
+    r = requests.get(baseurl + prodsurl + str(page))
 
     # Parse HTML from response - zero in on the cards with the content I want, avoid getting menu links etc
     soup = BeautifulSoup(r.content, 'html.parser')
@@ -31,36 +40,47 @@ for page in range(0, pages):
     for a in content.find_all('a'):
         link = a.get('href')
         if link.startswith('/'):
-            plinks.append('https://www.eurocontrol.int' + link)
+            plinks.append(baseurl + link)
+            # Find product type in URL
+            parsedlink = urlparse(link).path
+            subtype = parsedlink.rpartition('/')[0]
+            ptype.append(subtype)
             
+productsdf = pd.DataFrame({col1: plinks, col2: ptype})
+productsdf.to_excel("output.xlsx", sheet_name="From website", index=False)
+
 # Fetch page info for each product/tool based on previously found URL
 for plink in plinks:
     # 1. Fetch relevant page content
     pr = requests.get(plink)
-    psoup = BeautifulSoup(pr.content, 'html.parser')
-    print(plink)
 
-    # 2. Find title
-    print(psoup.find('h1'))
-    print(psoup.find('h1').text)
-    title = psoup.find('h1').text
-    pnames.append(title)
+    # 2. Check for success
+    if pr.ok:
+        psoup = BeautifulSoup(pr.content, 'html.parser')
 
-    # 3. Find activities - 0 to multiple
-    acts = []
-    allactlinks = psoup.find('div', class_ = 'content--header__second').find_all('a')
+        # 2.1 Find title
+        title = psoup.find('h1').text
+        pnames.append(title)
 
-    for actlink in allactlinks:
-        acts.append(actlink.text)
+        # 2.2. Find activities - 0 to multiple
+        acts = []
+        allactlinks = psoup.find('div', class_ = 'content--header__second').find_all('a')
 
-    pacts.append(acts)
+        for actlink in allactlinks:
+            acts.append(actlink.text)
 
-    time.sleep(5) 
+        pacts.append(acts)
 
-col1 = "Link"
-col2 = "Page title"
-col3 = "Activities"
+        # 2.3. Add line to Excel
 
-productsdf = pd.DataFrame({col1: plinks, col2: pnames, col3: pacts})
-productsdf = pd.DataFrame({col1: plinks})
-productsdf.to_excel("output.xlsx", sheet_name="From website", index=False)
+    else:
+        # 3. If fail: print error
+        print('Error: ' + pr.status_code)
+
+    # Random naps to avoid getting blocked
+    sleep(randint(1,5))
+
+productsdf[col2] = ptype
+productsdf[col3] = pnames
+productsdf[col4] = pacts
+productsdf.to_excel("webcatalogue.xlsx", sheet_name="From website", index=False)
