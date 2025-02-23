87a4f186cb8384d7444a635c0fe26de4736b81d6
Livia Bottoni <livia.bottoni@gmail.com>
2025-02-23 21:45:51 +0100

    adding naptime to avoid cloudflare blocking errors

--- hello.py
+++ hello.py
@@ -4,6 +4,8 @@ import requests
 from bs4 import BeautifulSoup
 # Pandas for writing DataFrame
 import pandas as pd
+# Time to insert delays - otherwise, too many requests get blocked
+import time
 
 # Start from product catalogue page with pagination parameter
 url = 'https://www.eurocontrol.int/what-we-offer?page='
@@ -37,23 +39,28 @@ for plink in plinks:
     pr = requests.get(plink)
     psoup = BeautifulSoup(pr.content, 'html.parser')
     print(plink)
+
     # 2. Find title
-    #title = psoup.find('h1').text
-    #pnames.append(title)
+    print(psoup.find('h1'))
+    print(psoup.find('h1').text)
+    title = psoup.find('h1').text
+    pnames.append(title)
 
     # 3. Find activities - 0 to multiple
-    #acts = []
-    #allactlinks = psoup.find('div', class_ = 'content--header__second').find_all('a')
+    acts = []
+    allactlinks = psoup.find('div', class_ = 'content--header__second').find_all('a')
 
-    #for actlink in allactlinks:
-    #    acts.append(actlink.text)
+    for actlink in allactlinks:
+        acts.append(actlink.text)
 
-    #pacts.append(acts)
+    pacts.append(acts)
+
+    time.sleep(5) 
 
 col1 = "Link"
-#col2 = "Page title"
-#col3 = "Activities"
+col2 = "Page title"
+col3 = "Activities"
 
-#productsdf = pd.DataFrame({col1: plinks, col2: pnames, col3: pacts})
+productsdf = pd.DataFrame({col1: plinks, col2: pnames, col3: pacts})
 productsdf = pd.DataFrame({col1: plinks})
 productsdf.to_excel("output.xlsx", sheet_name="From website", index=False)
